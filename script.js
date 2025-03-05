9f082f5bab4203a84c11ba2ec4127685a55a7669
Arkoprovo Gupta <arkoprovogupta253@gmail.com>
2025-03-06 04:36:56 +0530

    Added backend fetch to frontend

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 document.getElementById('notifyBtn')?.addEventListener('click', () => {
     alert('You will be notified when the product is back in stock!');
 });
+
+// Fetch data from the backend
+fetch("https://aec-ubbb.onrender.com/api/test")  // Use your Render backend URL
+    .then(response => response.json())
+    .then(data => {
+        console.log("Response from backend:", data);
+        document.getElementById('backendMessage').innerText = data.message;
+    })
+    .catch(error => console.error("Error fetching from backend:", error));
